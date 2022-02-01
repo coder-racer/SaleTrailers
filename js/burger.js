fd868e46a4229062c31145997eb356a3ce5ca3b8
atom <vlad.v428@gmail.com>
2022-02-02 05:43:44 +0600

    add list

--- js/burger.js
+++ js/burger.js
@@ -1,4 +1,5 @@
 let Burger = false;
+let Filter = false;
 function burger_click(){
     Burger = !Burger;
     if (Burger) {
@@ -9,4 +10,15 @@ function burger_click(){
         document.querySelector(".burger_icon").classList.remove('burger_icon_active')
         document.querySelector(".burger_menu").classList.remove('burger_menu_active')
     }
+}
+
+function onFilter(){
+    Filter = !Filter;
+    if (Filter) {
+        document.querySelector(".order-filter").classList.add('order-filter-active')
+    }
+    else {
+        document.querySelector(".order-filter").classList.remove('order-filter-active')
+    }
+
 }
